031a2e60ce3ed0462798af131c06280753f84d27
GGWithGitHub <71557801+GGWithGitHub@users.noreply.github.com>
2024-12-23 18:51:37 +0530

    Add files via upload

---
+++ Repository.cs
@@ -0,0 +1,284 @@
+﻿using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PS.DataCore.Core
+{
+    public class Repository<TEntity, TContext> : IRepository<TEntity, TContext>
+     where TEntity : class
+     where TContext : DbContext
+    {
+
+        protected readonly DbSet<TEntity> dbSet;
+        protected readonly TContext dbContext;
+
+        public Repository(TContext dbContext)
+        {
+            this.dbContext = dbContext;
+            dbSet = this.dbContext.Set<TEntity>();
+        }
+
+        public TContext DbContext => dbContext;
+
+        public virtual ValueTask<TEntity> GetByIdAsync(object id)
+        {
+            return dbSet.FindAsync(id);
+        }
+
+        public virtual TEntity GetById(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual IQueryable<TEntity> GetAll(bool disableTracking = false)
+        {
+            IQueryable<TEntity> query = null;
+            if (disableTracking)
+            {
+                query = dbSet.AsNoTracking().AsQueryable();
+            }
+            else
+            {
+                query = dbSet.AsQueryable();
+            }
+            return query;
+        }
+
+        public virtual TContext GetContext()
+        {
+            return this.dbContext;
+        }
+
+        public virtual void Create(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        /// <summary>
+        /// Does a hard delete of the entity.  Override this if you want to soft delete instead.
+        /// </summary>
+        public virtual void Delete(TEntity entity)
+        {
+            dbSet.Remove(entity);
+        }
+        public virtual void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+
+        public void RollBack()
+        {
+            var changedEntries = dbContext.ChangeTracker.Entries<TEntity>()
+                .Where(x => x.State != EntityState.Unchanged)
+                .ToList();
+
+            foreach (var entry in changedEntries)
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.CurrentValues.SetValues(entry.OriginalValues);
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                }
+            }
+        }
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+        public virtual Task BulkInsertAsync(IList<TEntity> entities)
+        {
+            if (dbContext.Database.IsSqlServer())
+            {
+                return dbContext.BulkInsertAsync<TEntity>(entities);
+            }
+            else
+            {
+                foreach (TEntity entity in entities)
+                {
+                    Add(entity);
+                }
+                return dbContext.SaveChangesAsync();
+            }
+        }
+
+        public virtual Task BulkDeleteAsync(IList<TEntity> entities)
+        {
+            if (dbContext.Database.IsSqlServer())
+            {
+                return dbContext.BulkDeleteAsync<TEntity>(entities);
+            }
+            else
+            {
+                foreach (TEntity entity in entities)
+                {
+                    Delete(entity);
+                }
+                return dbContext.SaveChangesAsync();
+            }
+        }
+
+        public IQueryable<TEntity> Query(string sql, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Search(params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Single(Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool disableTracking = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(TEntity entity)
+        {
+            dbContext.Add<TEntity>(entity);
+        }
+
+        public void Add(params TEntity[] entities)
+        {
+            entities.ToList().ForEach(x => Add(x));
+        }
+
+        public void Add(IEnumerable<TEntity> entities)
+        {
+            entities.ToList().ForEach(x => Add(x));
+        }
+
+        public void Delete(object id)
+        {
+            var entity = GetById(id);
+            Delete(entity);
+        }
+
+        public void Delete(params TEntity[] entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            dbContext.Update<TEntity>(entity);
+        }
+
+        public void Update(params TEntity[] entities)
+        {
+            entities.ToList().ForEach(x => Update(x));
+        }
+
+        public void Update(IEnumerable<TEntity> entities)
+        {
+            entities.ToList().ForEach(x => Update(x));
+        }
+
+        public void Dispose()
+        {
+            dbContext.Dispose();
+        }
+
+        public IQueryable<TEntity> Filter(Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool disableTracking = false)
+        {
+            IQueryable<TEntity> query = null;
+            if (disableTracking)
+            {
+                query = dbSet.AsNoTracking().AsQueryable();
+            } else
+            {
+                query = dbSet.AsQueryable();
+            }
+
+            query = query.Where(predicate);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            if (include != null)
+                query = include(query);
+
+            return query;
+        }
+
+        public Task<List<TEntity>> FilterAsync(Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool disableTracking = false)
+        {
+            return Filter(predicate, orderBy, include, disableTracking).ToListAsync();
+        }
+    }
+
+    public interface IRepository<TEntity, TContext> : IDisposable
+        where TEntity : class
+        where TContext : DbContext
+    {
+        TContext DbContext { get; }
+
+        IQueryable<TEntity> Query(string sql, params object[] parameters);
+
+        TEntity Search(params object[] keyValues);
+
+        TEntity Single(Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool disableTracking = false);
+
+        IQueryable<TEntity> Filter(Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool disableTracking = false);
+
+        Task<List<TEntity>> FilterAsync(Expression<Func<TEntity, bool>> predicate = null,
+           Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+           Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+           bool disableTracking = false);
+        void Add(TEntity entity);
+        void Add(params TEntity[] entities);
+        void Add(IEnumerable<TEntity> entities);
+
+        void Delete(TEntity entity);
+        void Delete(object id);
+        void Delete(params TEntity[] entities);
+        void Delete(IEnumerable<TEntity> entities);
+        void DeleteRange(IEnumerable<TEntity> entities);
+        void Update(TEntity entity);
+        void Update(params TEntity[] entities);
+        void Update(IEnumerable<TEntity> entities);
+        ValueTask<TEntity> GetByIdAsync(object id);
+        TEntity GetById(object id);
+        void Create(TEntity entity);
+        Task BulkInsertAsync(IList<TEntity> entities);
+        Task BulkDeleteAsync(IList<TEntity> entities);
+        IQueryable<TEntity> GetAll(bool disableTracking = false);
+        TContext GetContext();
+        void RollBack();
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ UnitOfWork.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using PS.DataCore.Models;
+using StoredProcedureEFCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace PS.DataCore.Core
+{
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext
+    {
+        private readonly TContext dbContext;
+
+        public UnitOfWork(TContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public int Commit()
+        {
+            return dbContext.SaveChanges();
+        }
+
+        public Task<int> CommitAsync()
+        {
+            return dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> CompleteAsync(int contactId = 0)
+        {
+            var entities = dbContext.ChangeTracker.Entries()
+                .Where(e => e.Entity is EntityBase && (e.State == EntityState.Added || e.State == EntityState.Modified))
+                .Select(e => e.Entity as EntityBase);
+
+            foreach (var entity in entities)
+            {
+                entity?.SaveStandardFields(contactId); // replace 0 with your contactID
+            }
+            try
+            {
+                return await dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                // Log the exception
+                throw new Exception("An error occurred while saving changes.", ex.InnerException);
+                throw;
+            }
+        }
+
+        public void Rollback()
+        {
+            dbContext.Database.RollbackTransaction();
+        }
+
+        public IDbContextTransaction BeginTransaction()
+        {
+            return dbContext.Database.BeginTransaction(System.Data.IsolationLevel.Snapshot);
+        }
+
+        public Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return dbContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.Snapshot);
+        }
+
+        public async Task<IList<TOutput>> ExecuteStoredProcedureQueryAsync<TOutput>(string storedProcedure,
+            SqlParameter[] parameters = null) where TOutput : class, new()
+        {
+            IList<TOutput> rows = null;
+            var sp = dbContext.LoadStoredProc(storedProcedure);
+            if (parameters != null && parameters.Any())
+            {
+                foreach (var parameter in parameters)
+                {
+                    if ((parameter.DbType == DbType.DateTime || parameter.DbType == DbType.DateTime2) && parameter.Value != null)
+                    {
+                        sp.AddParam<DateTime>(parameter.ParameterName, (DateTime)parameter.Value);
+                    }
+                    else
+                    {
+                        sp.AddParam<string>(parameter.ParameterName, parameter.Value?.ToString());
+                    }
+                }
+            }
+
+            await sp.ExecAsync(async r => rows = await r.ToListAsync<TOutput>());
+
+            return rows;
+        }
+        public async Task<TOutput> ExecuteScalarStoredProcedureQueryAsync<TOutput>(string storedProcedure,
+            SqlParameter[] parameters = null)
+        {
+            var sp = dbContext.LoadStoredProc(storedProcedure);
+            if (parameters != null && parameters.Any())
+            {
+                foreach (var parameter in parameters)
+                {
+                    sp.AddParam<string>(parameter.ParameterName, parameter.Value.ToString());
+                }
+            }
+            TOutput result = default(TOutput);
+            await sp.ExecScalarAsync<TOutput>(r => result = r);
+
+            return result;
+        }        
+
+        public async Task ExecuteNonQueryAsync(string storedProcedure, SqlParameter[] parameters = null)
+        {
+            var sp = dbContext.LoadStoredProc(storedProcedure);
+            
+            if (parameters != null && parameters.Any())
+            {
+                foreach (var parameter in parameters)
+                {
+                    if (parameter.Value != null)
+                    {
+                        sp.AddParam<string>(parameter.ParameterName, parameter.Value.ToString());
+                    } else
+                    {
+                        sp.AddParam(parameter);
+                    }
+                }
+            }
+            await sp.ExecNonQueryAsync();
+        }
+
+        public async Task ExecNonQueryWithOutputPraramAsync(string storedProcedure, SqlParameter[] parameters)
+        {
+            var sp = dbContext.LoadStoredProc(storedProcedure);
+
+            if (parameters != null && parameters.Any())
+            {
+                foreach (var parameter in parameters)
+                {
+                    sp.AddParam(parameter);
+                }
+            }
+            await sp.ExecNonQueryAsync();
+        }
+
+        public void Dispose()
+        {
+            dbContext.Dispose();
+        }
+        public DbContext GetContext()
+        {
+            return dbContext;
+        }
+    }
+
+    public interface IUnitOfWork<TDbContext> : IDisposable where TDbContext : DbContext
+    {
+        int Commit();
+        Task<int> CommitAsync();
+        Task<int> CompleteAsync(int contactId = 0);
+        IDbContextTransaction BeginTransaction();
+        Task<IDbContextTransaction> BeginTransactionAsync();
+        Task<IList<TOutput>> ExecuteStoredProcedureQueryAsync<TOutput>(string storedProcedure,
+            params SqlParameter[] parameters) where TOutput : class, new();
+        Task<TOutput> ExecuteScalarStoredProcedureQueryAsync<TOutput>(string storedProcedure,
+           SqlParameter[] parameters = null);
+        Task ExecuteNonQueryAsync(string storedProcedure, SqlParameter[] parameters = null);
+        void Rollback();
+        Task ExecNonQueryWithOutputPraramAsync(string storedProcedure, SqlParameter[] parameters);
+        DbContext GetContext();
+    }
+}
